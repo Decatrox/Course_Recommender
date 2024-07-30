0aff381cdf1cf86575c5a254cbc1723bd700f281
Decatrox <omarwael7@icloud.com>
2024-07-30 18:39:04 +0300

    Excluded DataSource in @SpringBootApplication to run without a database

--- src/main/java/com/sumerge/course_recommender/CourseRecommenderApplication.java
+++ src/main/java/com/sumerge/course_recommender/CourseRecommenderApplication.java
@@ -2,8 +2,9 @@ package com.sumerge.course_recommender;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CourseRecommenderApplication {
 
     public static void main(String[] args) {
