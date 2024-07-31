75bc1f4b82161c3e9e2501d9d431819c427b604b
Decatrox <omarwael7@icloud.com>
2024-07-31 21:21:08 +0300

    Excluded DataSource in @SpringBootApplication to run without a database & added controller example code to test (not my code)

--- src/main/java/com/sumerge/course_recommender/CourseRecommenderApplication.java
+++ src/main/java/com/sumerge/course_recommender/CourseRecommenderApplication.java
@@ -3,6 +3,7 @@ package com.sumerge.course_recommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CourseRecommenderApplication {
@@ -12,3 +13,15 @@ public class CourseRecommenderApplication {
     }
 
 }
+
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//public class CourseRecommenderApplication {
+//
+//    public static void main(String[] args) {
+//
+//        ApplicationContext context = SpringApplication.run(CourseRecommenderApplication.class, args);
+//        CourseService service = context.getBean(CourseService.class);
+//        service.getRecommendedCourses();
+//    }
+//
+//}
