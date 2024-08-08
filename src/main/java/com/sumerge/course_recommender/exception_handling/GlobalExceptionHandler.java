4ae17469a38ec38e7f51bbf97e08487a25e9bbd2
Decatrox <omarwael7@icloud.com>
2024-08-08 07:42:03 +0300

    Added Exception Handling to return just the error message I specified from the validation

--- src/main/java/com/sumerge/course_recommender/exception_handling/GlobalExceptionHandler.java
+++ src/main/java/com/sumerge/course_recommender/exception_handling/GlobalExceptionHandler.java
@@ -2,44 +2,27 @@ package com.sumerge.course_recommender.exception_handling;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(jakarta.validation.ConstraintViolationException.class)
-    public ResponseEntity<ApiError> handleConstraintViolationException(jakarta.validation.ConstraintViolationException ex, WebRequest request) {
-        ApiError apiError = new ApiError(
-                LocalDateTime.now()
-//                HttpStatus.BAD_REQUEST.value(),
-//                "Bad Request",
-//                ex.getConstraintViolations().iterator().next().getMessage(),
-//                request.getDescription(false)
-        );
-        return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
-
-    @ExceptionHandler(org.springframework.transaction.TransactionSystemException.class)
-    public ResponseEntity<ApiError> handleTransactionSystemException(org.springframework.transaction.TransactionSystemException ex, WebRequest request) {
-        Throwable cause = ex.getRootCause();
-        String message = (cause instanceof jakarta.validation.ConstraintViolationException) ?
-                ((jakarta.validation.ConstraintViolationException) cause).getConstraintViolations().iterator().next().getMessage() :
-                ex.getMessage();
-
-        ApiError apiError = new ApiError(
-                LocalDateTime.now()
-//                HttpStatus.INTERNAL_SERVER_ERROR.value(),
-//                "Internal Server Error",
-//                message,
-//                request.getDescription(false)
-        );
-        return new ResponseEntity<>(apiError, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
-
-    // You can add more exception handlers for other types of exceptions
 }
+
 
