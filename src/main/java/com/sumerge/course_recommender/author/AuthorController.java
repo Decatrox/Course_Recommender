dd8ccb3652e2619526e6dbab637cc7435bfa741b
Decatrox <omarwael7@icloud.com>
2024-08-12 09:32:09 +0300

    Added @ApiResponses and @Operation

--- src/main/java/com/sumerge/course_recommender/author/AuthorController.java
+++ src/main/java/com/sumerge/course_recommender/author/AuthorController.java
@@ -1,5 +1,8 @@
 package com.sumerge.course_recommender.author;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,11 +17,21 @@ public class AuthorController {
         this.authorService = authorService;
     }
 
+    @Operation(summary = "Add a new author")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Author added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid input"),
+    })
     @PostMapping
     public void addAuthor(@Valid @RequestBody AuthorPostDTO author) {
         authorService.addAuthor(author);
     }
 
+    @Operation(summary = "Get an author by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Author found"),
+            @ApiResponse(responseCode = "404", description = "Author not found"),
+    })
     @GetMapping("/{email}")
     public ResponseEntity<AuthorGetDTO> getAuthorByEmail(@PathVariable String email) {
         return ResponseEntity.ok(authorService.getAuthorByEmail(email));
--- src/main/java/com/sumerge/course_recommender/course/CourseController.java
+++ src/main/java/com/sumerge/course_recommender/course/CourseController.java
@@ -1,5 +1,8 @@
 package com.sumerge.course_recommender.course;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -16,27 +19,53 @@ public class CourseController {
 
     private final CourseService courseService;
 
-
+    @Operation(summary = "Get courses with pagination")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course page returned successfully"),
+            @ApiResponse(responseCode = "404", description = "Not enough courses in the database to reach the" +
+                    " requested page number"),
+    })
     @GetMapping("/paged/{pageNumber}")
     public ResponseEntity<Page<CourseGetDTO>> recommendCourse(@PathVariable int pageNumber) {
         return ResponseEntity.ok(courseService.getRecommendedCourses(pageNumber));
     }
 
+    @Operation(summary = "View a course by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course found"),
+            @ApiResponse(responseCode = "404", description = "Course not found"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CourseGetDTO> viewCourse(@PathVariable UUID id) {
         return ResponseEntity.ok(courseService.viewCourse(id));
     }
 
+    @Operation(summary = "Update a course by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course updated successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid course data"),
+            @ApiResponse(responseCode = "404", description = "Course not found"),
+    })
     @PutMapping("/{id}")
     public ResponseEntity<String> updateCourse(@PathVariable UUID id, @Valid @RequestBody CoursePostDTO course) {
         return ResponseEntity.ok(courseService.updateCourse(id, course));
     }
 
+    @Operation(summary = "Delete a course by ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Course deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "Course not found"),
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteCourse(@PathVariable UUID id) {
         return ResponseEntity.ok(courseService.deleteCourse(id));
     }
 
+    @Operation(summary = "Add a new course")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Course added successfully"),
+            @ApiResponse(responseCode = "400", description = "Invalid course data"),
+    })
     @PostMapping
     public ResponseEntity<String> addCourse(@Valid @RequestBody CoursePostDTO course) {
         return ResponseEntity.ok(courseService.addCourse(course));
